d3ec6318fb681ed51a2814f284a1460fce8ae9e5
Lauren Caliolio <lauren@imperium>
2016-06-20 01:21:39 -0400

    removing gitconfig from files variable

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,7 +11,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig bash_profile profile bashrc vimrc vim"    # list of files/folders to symlink in homedir
+files="bash_profile profile bashrc vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
 
