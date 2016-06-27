be21e76a5499d2a7b905318f854f173123c61c98
Lauren Caliolio <laurendc@gmail.com>
2016-06-26 20:36:13 -0400

    adding . to for loop for symblinking

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -15,20 +15,25 @@ files="bash_profile profile bashrc vimrc vim"    # list of files/folders to syml
 
 ##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+# create dot"files_old in homedir
+if [ -d $olddir ];
+then 
+    echo -n "Directory already exists, moving on"
+else
+    echo -n "Creating $olddir for backup of any existing dotfiles in ~ "
+    mkdir -p $olddir
+    echo -n "done"
+fi
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo -n "Changing to the $dir directory"
 cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.${file} ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/.${file} ~/.${file}
 done
